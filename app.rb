7fee1204674f2b04783deb49a6cb9504f3cb3425
Dorian Marié <dorian@dorianmarie.fr>
2020-07-28 14:49:27 +0200

    add heart rate

--- app.rb
+++ app.rb
@@ -4,14 +4,22 @@ require "sinatra"
 require "csv"
 require "json"
 
+ONE_MINUTE_IN_MS = 1000 * 60
+
 post "/delineation" do
   csv = CSV.new(params[:data][:tempfile])
 
   premature_p_waves = 0
   premature_qrs_complexes = 0
+  minimum_onset = nil
+  maximum_onset = nil
+  qrs_complexes = 0
 
   csv.each do |line|
     type, onset, offset, *tags = line
+
+    onset = onset.to_i
+    offset = offset.to_i
 
     if type == "P" && tags.include?("premature")
       premature_p_waves += 1
@@ -20,10 +28,25 @@ post "/delineation" do
     if type == "QRS" && tags.include?("premature")
       premature_qrs_complexes += 1
     end
+
+    if type == "QRS"
+      qrs_complexes += 1
+    end
+
+    if minimum_onset.nil? || onset < minimum_onset
+      minimum_onset = onset
+    end
+
+    if maximum_onset.nil? || onset > maximum_onset
+      maximum_onset = onset
+    end
   end
+
+  heart_rate = qrs_complexes / ((maximum_onset - minimum_onset) / ONE_MINUTE_IN_MS)
 
   JSON.pretty_generate({
     premature_p_waves: premature_p_waves,
     premature_qrs_complexes: premature_qrs_complexes,
+    heart_rate: heart_rate,
   })
 end
