e18ffe23e2590e4d716cf2e064c626155d86bd03
Dorian Marié <dorian@dorianmarie.fr>
2020-07-28 16:32:41 +0200

    small changes to make the code clearer

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ require "sinatra"
 require "csv"
 require "json"
 
-ONE_MINUTE_IN_MS = 1000 * 60
 ONE_SECOND_IN_MS = 1000
+ONE_MINUTE_IN_MS = ONE_SECOND_IN_MS * 60
 
 post "/delineation" do
-  if !params[:data] || !params[:data][:tempfile]
+  unless params[:data] && params[:data][:tempfile]
     status :bad_request
     return { error: "data file must be present" }.to_json
   end
@@ -24,8 +24,8 @@ post "/delineation" do
 
   premature_p_waves = 0
   premature_qrs_complexes = 0
-  minimum_onset = nil
-  maximum_onset = nil
+  minimum_qrs_onset = nil
+  maximum_qrs_onset = nil
   qrs_complexes = 0
   last_heart_beat_at = nil
   minimum_heart_rate = nil
@@ -42,35 +42,40 @@ post "/delineation" do
 
     qrs_complexes += 1
     premature_qrs_complexes += 1 if tags.include?("premature")
-    minimum_onset = onset if minimum_onset.nil? || onset < minimum_onset
-    maximum_onset = onset if maximum_onset.nil? || onset > maximum_onset
+    minimum_qrs_onset = onset if minimum_qrs_onset.nil? || onset < minimum_qrs_onset
+    maximum_qrs_onset = onset if maximum_qrs_onset.nil? || onset > maximum_qrs_onset
 
+    # when encountering the first qrs complex
     if last_heart_beat_at.nil?
       last_heart_beat_at = onset
-    else
-      minutes = (onset - last_heart_beat_at).to_f / ONE_MINUTE_IN_MS
-      heart_rate = (1 / minutes).to_i
-      last_heart_beat_at = onset
+      next
+    end
 
-      if minimum_heart_rate.nil? || minimum_heart_rate[:rate] > heart_rate
-        minimum_heart_rate = {
-          rate: heart_rate,
-          starts_at: starts_at + (onset / ONE_SECOND_IN_MS)
-        }
-      end
+    # minutes between the last heart beat and the current onset
+    minutes = (onset - last_heart_beat_at).to_f / ONE_MINUTE_IN_MS
+    # heart rate is in beats per minutes
+    heart_rate = (1 / minutes).to_i
+    last_heart_beat_at = onset
 
-      if maximum_heart_rate.nil? || maximum_heart_rate[:rate] < heart_rate
-        maximum_heart_rate = {
-          rate: heart_rate,
-          starts_at: starts_at + (onset / ONE_SECOND_IN_MS)
-        }
-      end
+    if minimum_heart_rate.nil? || minimum_heart_rate[:rate] > heart_rate
+      minimum_heart_rate = {
+        rate: heart_rate,
+        starts_at: starts_at + (onset / ONE_SECOND_IN_MS)
+      }
+    end
+
+    if maximum_heart_rate.nil? || maximum_heart_rate[:rate] < heart_rate
+      maximum_heart_rate = {
+        rate: heart_rate,
+        starts_at: starts_at + (onset / ONE_SECOND_IN_MS)
+      }
     end
   end
 
+  # number of heart beats encountered divided by the duration of the ECG
   mean_heart_rate = (
     (qrs_complexes - 1) /
-    ((maximum_onset - minimum_onset).to_f / ONE_MINUTE_IN_MS)
+    ((maximum_qrs_onset - minimum_qrs_onset).to_f / ONE_MINUTE_IN_MS)
   )
 
   JSON.pretty_generate(
